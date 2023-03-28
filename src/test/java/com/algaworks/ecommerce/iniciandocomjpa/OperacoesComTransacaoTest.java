3c1194d268929b6f1d9455af5c262f7798678237
joab.santos <desenvolvedor.suporte@gmail.com>
2023-03-28 15:35:55 -0300

    EJPA - 02.09 - inserindo o primeiro objeto com persist

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -1,9 +1,35 @@
 package com.algaworks.ecommerce.iniciandocomjpa;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 public class OperacoesComTransacaoTest extends EntityManagerTest {
+
+    @Test
+    @DisplayName("inserir o primeiro objeto")
+    public void inserirOPrimeiroObjeto(){
+        Produto produto = new Produto();
+
+        produto.setId(2L);
+        produto.setNome("Câmera Canon");
+        produto.setDescricao("A melhor definição para suas fotos");
+        produto.setPreco(new BigDecimal(5_000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assertions.assertNotNull(produtoVerificacao);
+    }
+
 
     @Test
     public void abrirEFcharATransacao() {
