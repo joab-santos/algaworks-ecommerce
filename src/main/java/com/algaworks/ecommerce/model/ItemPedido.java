fa3a5e7ca5ef8768f74d8d8946d29a6761c4cde9
joab.santos <desenvolvedor.suporte@gmail.com>
2023-04-21 20:09:54 -0300

    EJPA - 04.03 - exercicio relacionamento muitos para um

--- src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
+++ src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
@@ -19,11 +19,13 @@ public class ItemPedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "pedido_id")
-    private Long pedidoId;
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
 
-    @Column(name = "produto_id")
-    private Long produtoId;
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
 
     @Column(name = "preco_produto")
     private BigDecimal precoProduto;
--- src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToOne.java
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToOne.java
@@ -1,13 +1,12 @@
 package com.algaworks.ecommerce.relacionamentos;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Cliente;
-import com.algaworks.ecommerce.model.Pedido;
-import com.algaworks.ecommerce.model.StatusPedido;
+import com.algaworks.ecommerce.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class RelacionamentoManyToOne extends EntityManagerTest {
@@ -30,6 +29,34 @@ public class RelacionamentoManyToOne extends EntityManagerTest {
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
         Assertions.assertNotNull(pedidoVerificacao.getCliente());
+
+    }
+
+    @Test
+    public void verificaRelacionamentoItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1L);
+        Produto produto = entityManager.find(Produto.class, 1L);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1L);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        Assertions.assertNotNull(itemPedidoVerificacao.getPedido());
+        Assertions.assertNotNull(itemPedidoVerificacao.getProduto());
 
     }
 }
