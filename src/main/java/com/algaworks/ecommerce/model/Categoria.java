1ccba0f826c6ce7c560f2395b8c12213505d0a90
joab.santos <desenvolvedor.suporte@gmail.com>
2023-04-21 15:31:40 -0300

    EJPA - 03.09. Configurando a geração de identificador com @TableGenerator

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -14,8 +14,12 @@ public class Categoria {
 
     @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name = "seq", sequenceName = "sequencias_chave_primaria", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+    @TableGenerator(name = "tabela", table = "hibernate_sequences",
+            pkColumnName = "sequence_name",
+            pkColumnValue = "categoria",
+            valueColumnName = "next_val",
+            allocationSize = 1)
     private Long id;
 
     private String nome;
